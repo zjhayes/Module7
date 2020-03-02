f6dc45cc7f48888cf73fabb310c10c1db051821c
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-01 22:44:52 -0600

    Range tests fail

--- test_collections/test_basic_list_exception.py
+++ test_collections/test_basic_list_exception.py
@@ -9,6 +9,16 @@ class TestListException(unittest.TestCase):
         with self.assertRaises(ValueError):
             topic1.make_list()
 
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value=0)
+    def test_make_list_below_range(self, input):
+        with self.assertRaises(ValueError):
+            topic1.make_list()
+
+    @patch('fun_with_collections.basic_list_exception.get_input', return_value=101)
+    def test_make_list_above_range(self, input):
+        with self.assertRaises(ValueError):
+            topic1.make_list()
+
 
 if __name__ == '__main__':
     unittest.main()
