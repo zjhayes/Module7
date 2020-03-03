2eedd4da448805503b3561205881f95795d36429
Zachary Hayes <zachary.j.hayes@gmail.com>
2020-03-02 22:49:57 -0600

    Sort tests pass

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -1,8 +1,13 @@
 # Zachary Hayes
+import array as arr
 
 
 def sort_array(the_array):
-    the_array.sort()        # No return statement because arrays are passed by reference.
+    sorted_list = the_array.tolist().sort()
+    sorted_array = arr.array('d', sorted_list)
+    return sorted_array
+    # I used a return because even though arrays are passed by reference, I had to convert it to a list because
+    # apparently arrays don't have sort.
 
 
 def search_array(the_array, to_search):
